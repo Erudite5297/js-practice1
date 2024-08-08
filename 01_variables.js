dbc2eb673a93ca29cc1b93b7919fffd3ffde3270
Moirangthem Erudite <126459492+Erudite5297@users.noreply.github.com>
2024-08-08 09:42:18 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,25 @@
+moirangthem = "You can declare without any variable name too"
+const accountID = 141553
+let accountEmail = "moirangthemsumanta64@gmial.com"
+var accountPassword = "12345"
+accountCity = "Imphal West"
+hello = "Erudite, look here also you declare just by saying 'hello' "
+let accountState;
+
+// accountID = 2
+
+accountEmail = "loya@gmail.com"
+accountPassword = "02032004"
+accountCity = "Imphal East"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountID);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+console.table([accountID, accountEmail]);
+console.log(moirangthem);
+
+console.log(hello);
+
